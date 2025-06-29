182e5f69179075ff1023bea84bb85016cde866e7
Hemant Sharma <hemantsharma4th@gmail.com>
2025-06-29 10:58:38 +0530

    Handling ResourceNotFoundException (Globally)

---
+++ src/main/java/com/telusko/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.telusko.exceptions;
+
+import com.telusko.paylods.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException e){
+        String message =  e.getMessage();
+      return   new ResponseEntity<>(new ApiResponse(message,false), HttpStatus.NOT_FOUND);
+    }
+}
